4a4bd501837e1ab0bcadfbb42c6df957d2b91534
TingKai911 <tingkai911@gmail.com>
2020-10-19 07:03:50 +0800

    add to cart functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -18,7 +18,7 @@ const cartItemsFromStorage = localStorage.getItem("cartItems") ?
   JSON.parse(localStorage.getItem("cartItems")) : [];
 
 const initialState = {
-  cart: { cartItemsFromStorage }
+  cart: { cartItems: cartItemsFromStorage }
 };
 
 const middleware = [thunk];
