8a3ede234f5072ddd0f99cd5e625a662ba961df3
TingKai911 <tingkai911@gmail.com>
2020-11-03 07:28:30 +0800

    order details reducer & action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -2,6 +2,9 @@ import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
 } from "../constants/orderConstants";
 import axios from "axios";
 
@@ -39,6 +42,44 @@ export const createOrder = (order) => async (dispatch, getState) => {
         : error.message;
     dispatch({
       type: ORDER_CREATE_FAIL,
+      payload: message,
+    });
+  }
+};
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_DETAILS_REQUEST,
+    });
+
+    // Get the token
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    // To send the content type and the token in the header
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    // Send request to get the order by id
+    const { data } = await axios.get(`/api/orders${id}`, config);
+    console.log(data);
+
+    dispatch({
+      type: ORDER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
       payload: message,
     });
   }
