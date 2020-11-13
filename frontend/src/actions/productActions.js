d71a02a733afcda444f3130219b86ceff30402d1
TingKai911 <tingkai911@gmail.com>
2020-11-14 07:30:44 +0800

    admin create product

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -9,6 +9,9 @@ import {
   PRODUCT_DELETE_REQUEST,
   PRODUCT_DELETE_SUCCESS,
   PRODUCT_DELETE_FAIL,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_SUCCESS,
+  PRODUCT_CREATE_FAIL,
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
@@ -74,6 +77,44 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
         : error.message;
     dispatch({
       type: PRODUCT_DELETE_FAIL,
+      payload: message,
+    });
+  }
+};
+
+export const createProduct = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_CREATE_REQUEST,
+    });
+
+    // Get the token
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    // To send the content type and the token in the header
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    // Send post request to create a product (need to send an empty object here in a put request)
+    const { data } = await axios.post(`/api/products/`, {}, config);
+
+    dispatch({
+      type: PRODUCT_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({
+      type: PRODUCT_CREATE_FAIL,
       payload: message,
     });
   }
